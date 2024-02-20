1dfac4fe9feb52c6983a922a052e3b198020a232
funayamateppei <t.funayama0217@gmail.com>
2024-02-20 11:10:25 +0900

    textであれば条件分岐でスレッド投稿orスレッド返信できるようにした

--- app/Http/Controllers/Api/NotifyMessageController.php
+++ app/Http/Controllers/Api/NotifyMessageController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class NotifyMessageController extends Controller
 {
@@ -17,26 +16,62 @@ class NotifyMessageController extends Controller
         $userToken = env("SLACK_USER_TOKEN");
         $botToken = env("SLACK_BOT_TOKEN");
 
+        $chatId = $request->input('entity.chatId');
         $thread = Http::withHeaders([
             'Content-Type' => 'application/json',
             'Authorization' => "Bearer $userToken",
         ])->get("https://slack.com/api/search.messages", [
-            'query' => 'from:MessageTest in:#スレッド投稿 "test1"',
+            'query' => "from:MessageTest in:#スレッド投稿 " . "chat_id: $chatId",
             'count' => 1
         ])->json();
-        Log::debug($thread);
 
-
-        $response = Http::withHeaders([
-            'Content-Type' => 'application/json',
-            'Authorization' => "Bearer $botToken",
-        ])->post('https://slack.com/api/chat.postMessage', [
-            'channel' => "C06K407C4V9",
-            'text' => $request->input('entity.plainText'),
-            // 'thread_ts' => "1708330728.165789",
-        ]);
-
-        // Log::debug($response);
-
+        if (!$thread) {
+            Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Authorization' => "Bearer $botToken",
+            ])->post('https://slack.com/api/chat.postMessage', [
+                'channel' => "C06K407C4V9",
+                'blocks' => [
+                    [
+                        "type" => "header",
+                        "text" => [
+                            "type" => "plain_text",
+                            "text" => "お問い合わせ"
+                        ]
+                    ],
+                    [
+                        "type" => "section",
+                        "fields" => [
+                            [
+                                "type" => "mrkdwn",
+                                "text" => "*テナント名:*\n" . $request->input('refers.user.profile.tenantName')
+                            ],
+                            [
+                                "type" => "mrkdwn",
+                                "text" => "*ユーザー名:*\n" . $request->input('refers.user.name')
+                            ]
+                        ]
+                    ],
+                    [
+                        "type" => "section",
+                        "fields" => [
+                            [
+                                "type" => "plain_text",
+                                "text" => "chat_id: " . $request->input('entity.chatId')
+                            ],
+                        ]
+                    ]
+                ]
+            ]);
+        } else {
+            Http::withHeaders([
+                'Content-Type' => 'application/json',
+                'Authorization' => "Bearer $botToken",
+            ])->post('https://slack.com/api/chat.postMessage', [
+                'channel' => "C06K407C4V9",
+                'text' => $request->input('entity.plainText'),
+                'thread_ts' => $thread['messages']['matches'][0]['ts'],
+            ]);
+        }
     }
 }
