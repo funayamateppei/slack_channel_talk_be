ebbcddcc3895254b40c4bc3fd2338fc7231f5996
funayamateppei <t.funayama0217@gmail.com>
2024-02-20 09:17:59 +0900

    スレッド投稿機能実装

--- app/Http/Controllers/Api/NotifyMessageController.php
+++ app/Http/Controllers/Api/NotifyMessageController.php
@@ -4,16 +4,30 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 class NotifyMessageController extends Controller
 {
-    public function notifyCreateChat(Request $request)
+    public function notifyChatMessage(Request $request)
     {
-        ddd($request->all());
-    }
+        $type = $request->input('entity.personType');
+        if (!$type || !isset($type) || $type !== 'user') return;
 
-    public function notifyReply(Request $request)
-    {
-        ddd($request->all());
+
+
+
+
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer xoxb-6647197972901-6644324815110-UB5eWGxyTd3e9wjYsnro4yai',
+        ])->post('https://slack.com/api/chat.postMessage', [
+            'channel' => "C06K407C4V9",
+            'text' => $request->input('entity.plainText'),
+            // 'thread_ts' => "1708330728.165789",
+        ]);
+
+        // Log::debug($response);
+
     }
 }
