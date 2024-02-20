2b75e1d603aa96b571f31326b45918f7c97f3d9a
funayamateppei <t.funayama0217@gmail.com>
2024-02-20 12:28:07 +0900

    軽微な修正

--- app/Http/Controllers/Api/NotifyMessageController.php
+++ app/Http/Controllers/Api/NotifyMessageController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Models\Thread;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 
 class NotifyMessageController extends Controller
 {
